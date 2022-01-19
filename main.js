ac4cb627af273923bbee0822c6a2cdcb9e3037d3
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-19 16:50:17 -0300

    config express server

--- main.js
+++ main.js
@@ -3,14 +3,7 @@ const express =  require('express');
 const app = express();
 const { resolve } = require('path');
 
-app.use('/', 
-  express.static(
-    resolve(
-      __dirname,
-      './index.html'
-    )
-  )
-)
+app.use('/')
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
