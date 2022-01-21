1051e594b527e8b66ee5c28cc02fdf6257012070
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-21 13:14:55 -0300

    Mudando api key

--- public/js/keyAPI.js
+++
@@ -1,3 +0,0 @@
-const YOU_KEY_API = "563492ad6f9170000100000147a317a9132a4773acf8dd095c49abb0";
-
-export default YOU_KEY_API
---
+++ public/viewAPI.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight -5) {
+    console.log("Carregue mais aquivos");
+  }
+})
